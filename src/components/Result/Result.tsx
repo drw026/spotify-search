1b71f4a378ebfe74fb419596da83a77ae429d8a5
Andrew Reasoa <mail@andrewreasoa.com>
2022-04-07 18:58:16 +0200

    align loader to the center

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -3,6 +3,7 @@ import ArtistContainer from '../Artist/Artist.container';
 import TrackContainer from '../Track/Track.container';
 import useSearchResults from '../../hooks/useSearchResults';
 import Loader from '../Loader/Loader';
+import styles from './Result.module.scss';
 
 interface Props {
     query: string;
@@ -12,13 +13,17 @@ const Result = ({ query }: Props) => {
     const { error, data, isLoading } = useSearchResults(query);
 
     if (error) return null;
-    if (isLoading) return (<Loader />);
+    if (isLoading) return (
+        <div className={`${styles.result} ${styles.result__loading}`}>
+            <Loader />
+        </div>
+    );
 
     return (
-        <>
+        <div className={styles.result}>
             {data && data.artists.length > 0 && (<ArtistContainer artists={data.artists} />)}
             {data && data.tracks.length > 0 && (<TrackContainer tracks={data.tracks} />)}
-        </>
+        </div>
     );
 }
 
